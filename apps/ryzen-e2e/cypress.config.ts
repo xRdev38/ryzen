53485d05ea2fdac7cf032b905f677b7b0e7d8c8a
Rudy Nosile <rudy.nosile@educlever.com>
2023-09-29 10:51:56 +0200

    test: add e2e testing to ci

--- apps/ryzen-e2e/cypress.config.ts
+++ apps/ryzen-e2e/cypress.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
+export const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/ryzen-e2e/videos',
+  screenshotsFolder: '../../dist/cypress/apps/ryzen-e2e/screenshots',
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 60000,
+  requestTimeout: 60000,
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  supportFile: 'src/support/e2e.ts',
+};
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+    setupNodeEvents(on) {
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
+  },
 });
--- apps/ryzen-e2e/src/e2e/app.cy.ts
+++ apps/ryzen-e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('ryzen', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome ryzen');
+    getGreeting().contains('Welcome Rudy');
   });
 });
